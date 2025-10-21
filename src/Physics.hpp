bb755b35dd13dc760be25eca65a0f7cbbbf7e16d
BunnyBun <kethushanmukhareddy2126@gmail.com>
2025-10-21 18:34:44 +0530

    PHYSICS: Calculate the overlap during collision
    Useful for collision detection and resolution

--- src/Physics.hpp
+++ src/Physics.hpp
@@ -5,13 +5,19 @@
 namespace Physics {
 
     Vec2f GetOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b ) {
-        //TODO: return the overlap rectangle size of the bounding boxes of entity a and b
-        return Vec2f(0,0); 
+        //return the overlap rectangle size of the bounding boxes of entity a and b
+        Vec2f delta = {std::abs(a->get<CTransform>().pos.x-b->get<CTransform>().pos.x),std::abs(a->get<CTransform>().pos.y-b->get<CTransform>().pos.y)} ;
+        float ox = a->get<CBoundingBox>().halfSize.x + b->get<CBoundingBox>().halfSize.x - delta.x;
+        float oy = a->get<CBoundingBox>().halfSize.y + b->get<CBoundingBox>().halfSize.y - delta.y;
+        return Vec2f(ox,oy); 
     }
     
     Vec2f GetPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b ) {
-        //TODO: return the previous overlap rectangle size of the bounding boxes of entity a and b
+        //return the previous overlap rectangle size of the bounding boxes of entity a and b
         // previous overlap use entity's previous position
-        return Vec2f(0,0); 
+        Vec2f delta = {std::abs(a->get<CTransform>().prevPos.x-b->get<CTransform>().prevPos.x),std::abs(a->get<CTransform>().prevPos.y-b->get<CTransform>().prevPos.y)} ;
+        float ox = a->get<CBoundingBox>().halfSize.x + b->get<CBoundingBox>().halfSize.x - delta.x;
+        float oy = a->get<CBoundingBox>().halfSize.y + b->get<CBoundingBox>().halfSize.y - delta.y;
+        return Vec2f(ox,oy);
     }
 }
